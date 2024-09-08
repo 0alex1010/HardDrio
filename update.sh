4abafba22cd172f5d4ea060fe90ba15d9382331f
Alexer <robotalexjennings@gmail.com>
2024-09-07 23:26:01 -0500

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+echo "DrioUpdate"
+echo "_________________________________"
+echo "Updateing..."
+rm harddrio.sh
+wget https://raw.githubusercontent.com/0alex1010/HardDrio/main/harddrio.sh
+echo "Removed and updated harddrio.sh!"
+exit 0
