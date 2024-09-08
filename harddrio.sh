0d6c6415967eae5b284f2dad58c390d2c944e7d6
Alexer <robotalexjennings@gmail.com>
2024-09-07 23:17:59 -0500

    Create harddrio.sh

---
+++ harddrio.sh
@@ -0,0 +1,12 @@
+echo "HardDrio"
+echo "_____________________________"
+echo "Starting HardDrio bash shell.."
+cd harddrio
+clear
+echo "HardDrio bash shell started."
+bash
+clear
+echo "HardDrio"
+echo "_____________________________"
+echo "Exiting..."
+exit 0
