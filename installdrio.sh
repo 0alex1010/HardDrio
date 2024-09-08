3f45ba812bdbbf68bec1d103f7839ba13803ac38
Alexer <robotalexjennings@gmail.com>
2024-09-07 23:20:09 -0500

    Create installdrio.sh

---
+++ installdrio.sh
@@ -0,0 +1,10 @@
+clear
+echo "HardDrio"
+echo "__________________________"
+echo "Pulling files from github..."
+wget
+echo "Done. Exiting..."
+sleep 5
+clear
+echo "Thanks for using the HardDrio installer!"
+exit 1
