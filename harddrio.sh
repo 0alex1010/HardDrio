908dfe2959dfa18227e431c005a263fd4a7fefe9
Alexer <robotalexjennings@gmail.com>
2024-09-07 23:38:05 -0500

    Update harddrio.sh

--- harddrio.sh
+++ harddrio.sh
@@ -4,7 +4,7 @@ echo "Starting HardDrio bash shell.."
 cd harddrio
 clear
 echo "HardDrio bash shell started."
-wget https://github.com/0alex1010/HardDrio/blob/main/harddrio%20data/potato-land.jpg?raw=true
+wget -O potato.jpg https://github.com/0alex1010/HardDrio/blob/main/harddrio%20data/potato-land.jpg?raw=true
 bash
 rm potato-land.jpg
 clear
